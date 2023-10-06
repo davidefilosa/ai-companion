77021e9419f36dd40f42ef9d5137c619d6a4477a
Davide Filosa <davideclaypool@gmail.com!>
2023-10-06 09:32:53 +0200

    created API to update companion

---
+++ app/api/companion/[companionId]/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from "@/node_modules/next/server";
+import { request } from "http";
+import { currentUser } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { companionId: string } }
+) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, categoryId, instructions, seed } = body;
+
+    if (!params.companionId) {
+      return new NextResponse("Companion ID required", { status: 400 });
+    }
+
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse("Unauthorized.", { status: 401 });
+    }
+
+    if (
+      !src ||
+      !name ||
+      !description ||
+      !categoryId ||
+      !instructions ||
+      !seed
+    ) {
+      return new NextResponse("Missing required fileds.", { status: 400 });
+    }
+
+    const companion = await prismadb.companion.update({
+      where: { id: params.companionId },
+      data: {
+        src,
+        name,
+        description,
+        instructions,
+        seed,
+        userId: user.id,
+        userName: user.firstName,
+        categoryId,
+      },
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log("COMPANION CREATION", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
