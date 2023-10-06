3a39c484fe88244a81bb838325c1bd1ab6f61336
Davide Filosa <davideclaypool@gmail.com!>
2023-10-06 08:48:05 +0200

    created companion form

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   return (
     <div className="h-full">
       <Navbar />
-      <div className="hidden md:flex mt-16 w-20 fixed inset-y-0">
+      <div className="hidden md:flex mt-16 w-20 fixed h-full flex-col inset-y-0">
         <Sidebar />
       </div>
       <main className="md:pl-20 pt-16 h-full"> {children}</main>
