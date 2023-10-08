38f94d94739f6f2360ffa1320b87d0d14ba00b90
Davide Filosa <davideclaypool@gmail.com!>
2023-10-08 09:00:03 +0200

    fixed bug

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -36,6 +36,7 @@ const RootPage = async ({ searchParams }: RootPageProps) => {
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      {/* @ts-ignore*/}
       <Companions data={data} />
     </div>
   );
