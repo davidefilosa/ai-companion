51584dfe8558c0cc10ed5ee262e45a963eb6332e
Davide Filosa <davideclaypool@gmail.com!>
2023-10-08 09:05:08 +0200

    fixed bug

--- components/companions.tsx
+++ components/companions.tsx
@@ -1,21 +1,24 @@
-"use client";
-
+import Image from "next/image";
 import Link from "next/link";
 import { Companion } from "@prisma/client";
-import Image from "next/image";
-import { Card, CardFooter, CardHeader } from "./ui/card";
 import { MessagesSquare } from "lucide-react";
 
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+
 interface CompanionsProps {
-  data: (Companion & { _count: { messages: number } })[];
+  data: (Companion & {
+    _count: {
+      messages: number;
+    };
+  })[];
 }
 
-const Companions: React.FC<CompanionsProps> = ({ data }) => {
+export const Companions = ({ data }: CompanionsProps) => {
   if (data.length === 0) {
     return (
       <div className="pt-10 flex flex-col items-center justify-center space-y-3">
         <div className="relative w-60 h-60">
-          <Image fill src="/empty.png" alt="empty" className="grayscale" />
+          <Image fill className="grayscale" src="/empty.png" alt="Empty" />
         </div>
         <p className="text-sm text-muted-foreground">No companions found.</p>
       </div>
@@ -55,5 +58,3 @@ const Companions: React.FC<CompanionsProps> = ({ data }) => {
     </div>
   );
 };
-
-export default Companions;
